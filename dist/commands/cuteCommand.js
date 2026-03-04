761337fd63e7c211b4cad97f31232bb2a384df22
amitihere <amiti.aneesh1@gmail.com>
2026-03-04 11:20:59 +0530

    feat:implemented hel/cute commands

---
+++ dist/commands/cuteCommand.js
@@ -0,0 +1,34 @@
+class CuteCommand {
+    constructor(program) {
+        this.program = program;
+    }
+    register() {
+        this.program
+            .command('cute')
+            .action(() => this.cuteCall());
+    }
+    async cuteCall() {
+        try {
+            const response = await axios.get('https://dog.ceo/api/breeds/image/random');
+            const imageUrl = response.data.message;
+            console.log(`Here's a cute dog image for you: ${imageUrl}`);
+            console.log('Follow the link to see the image!');
+        }
+        catch (err) {
+            console.error('Error fetching cute image:', err);
+        }
+    }
+}
+// program
+//     .command('cuteness')
+//     .action(async () => {
+//         try{
+//             const response = await axios.get('https://dog.ceo/api/breeds/image/random');
+//             const imageUrl = response.data.message;
+//             console.log(`Here's a cute dog image for you: ${imageUrl}`);
+//             console.log('Follow the link to see the image!')
+//         }catch(err){
+//             console.error('Error fetching cute image:', err);
+//         }
+//     })
+module.exports = CuteCommand;
---
+++ dist/commands/helpCommand.js
@@ -0,0 +1,51 @@
+// program
+//     .command('help')
+//     .action(() => {
+//         console.log('Available commands:');
+//         console.log('greet <name>');
+//         console.log('add <num1> <num2>');
+//         console.log('subtract <num1> <num2>');
+//         console.log('multiply <num1> <num2>');
+//         console.log('divide <num1> <num2>');
+//         console.log('coin call');
+//         console.log('quote');
+//         console.log('joke');
+//         console.log('cuteness');
+//         console.log('help');
+//     });
+class HelpCommand {
+    constructor(program) {
+        this.program = program;
+    }
+    register() {
+        this.program
+            .command('help')
+            .action(() => this.helpCall());
+    }
+    async helpCall() {
+        console.log('Available commands:');
+        console.log('greet <name>');
+        console.log('add <num1> <num2>');
+        console.log('subtract <num1> <num2>');
+        console.log('multiply <num1> <num2>');
+        console.log('divide <num1> <num2>');
+        console.log('coin call');
+        console.log('quote');
+        console.log('joke');
+        console.log('cute');
+        console.log('help');
+    }
+}
+// program
+//     .command('cuteness')
+//     .action(async () => {
+//         try{
+//             const response = await axios.get('https://dog.ceo/api/breeds/image/random');
+//             const imageUrl = response.data.message;
+//             console.log(`Here's a cute dog image for you: ${imageUrl}`);
+//             console.log('Follow the link to see the image!')
+//         }catch(err){
+//             console.error('Error fetching cute image:', err);
+//         }
+//     })
+module.exports = HelpCommand;
